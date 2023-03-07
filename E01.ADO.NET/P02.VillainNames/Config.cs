aa0bb486896f0b6de8a769b5bf0802334ff0e032
Daniel Jordanov <114871054+DanielJordanovv@users.noreply.github.com>
2023-03-07 18:56:56 +0200

    Delete E01.ADO.NET directory

--- E01.ADO.NET/P02.VillainNames/Config.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace P02.VillainNames
-{
-    internal static class Config
-    {
-        public const string ConnectionString =
-            @"Server=.\SQLEXPRESS;Database=MinionsDB;Integrated Security=True;TrustServerCertificate=True";
-    }
-}
